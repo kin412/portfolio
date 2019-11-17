ca62a17652859c2904d060dd3dace1c109b21b22
kin412 <kin412@naver.com>
2019-11-17 19:13:06 +0900

    mybatis basic setting (list, read, delete, update)

---
+++ SpringBoardProject/src/main/java/org/kin/mapper/BoardMapper.java
@@ -0,0 +1,20 @@
+package org.kin.mapper;
+
+import java.util.List;
+
+import org.kin.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
---
+++ SpringBoardProject/src/test/java/org/kin/mapper/BoardMapperTests.java
@@ -0,0 +1,66 @@
+package org.kin.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kin.domain.BoardVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	/*
+	 * @Test public void testGetList() {
+	 * 
+	 * mapper.getList().forEach(board -> log.info(board));
+	 * 
+	 * }
+	 * 
+	 * @Test public void testInsert() {
+	 * 
+	 * BoardVO board = new BoardVO(); board.setTitle("새로운 글");
+	 * board.setContent("내용입니다."); board.setWriter("user01"); mapper.insert(board);
+	 * 
+	 * log.info(board); }
+	 */
+	
+	/*
+	 * @Test public void testRead() {
+	 * 
+	 * BoardVO board = mapper.read(5L);
+	 * 
+	 * log.info(board);
+	 * 
+	 * }
+	 */
+	
+	/*
+	 * @Test public void testDelete() { log.info("Delete Count : " +
+	 * mapper.delete(3L)); }
+	 */
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		board.setBno(61L);
+		board.setTitle("수정합니다 61");
+		board.setContent("내용 수정입니다. 61");
+		board.setWriter("user61");
+		
+		int count = mapper.update(board);
+		log.info("update count : " + count);
+	}
+
+}
